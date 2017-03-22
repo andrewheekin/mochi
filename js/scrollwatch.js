1657466b3915debfb20e106dbb62eedb70cc9cb7
Andrew Heekin <andrewheekin@gmail.com>
2017-03-22 14:27:54 -0400

    signup, description, example sections

--- js/scrollwatch.js
+++ js/scrollwatch.js
@@ -1,15 +1,25 @@
 let navbar = document.getElementById('navbar');
 let navHolder = document.getElementById('nav-holder');
+let logo = document.getElementById('logo');
+let mochi = document.getElementById('mochi');
+let mochiBox = document.getElementById('mochi-lightbox');
+
 
 // watch window scroll height and add/remove scrolled/unscrolled classes
 document.addEventListener('scroll', () => {
-  if (window.scrollY >= 30 && window.scrollY < 130) {
+  if (window.scrollY >= 20 && window.scrollY < 120) {
     navbar.className = 'navbar-scrolled';
     navHolder.className = 'nav-holder-scrolled';
+    logo.className = 'logo-scrolled';
+    mochi.className = 'mochi-scrolled';
+    mochiBox.className = 'mochi-lightbox-scrolled';
   }
   else if (window.scrollY < 30) {
     navbar.className = 'navbar';
     navHolder.className = 'nav-holder';
+    logo.className = 'logo';
+    mochi.className = 'mochi';
+    mochiBox.className = 'mochi-lightbox';
   }
 
 });

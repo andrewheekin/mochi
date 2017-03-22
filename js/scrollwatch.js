9f833bf11ccd8ba226a512140d383e954185426a
Andrew Heekin <andrewheekin@gmail.com>
2017-03-22 18:39:50 -0400

    added footer

--- js/scrollwatch.js
+++ js/scrollwatch.js
@@ -1,25 +1,49 @@
+import throttle from 'lodash/throttle';
+
 let navbar = document.getElementById('navbar');
 let navHolder = document.getElementById('nav-holder');
 let logo = document.getElementById('logo');
 let mochi = document.getElementById('mochi');
 let mochiBox = document.getElementById('mochi-lightbox');
+let learnMore = document.getElementById('learn-more');
 
 
 // watch window scroll height and add/remove scrolled/unscrolled classes
-document.addEventListener('scroll', () => {
-  if (window.scrollY >= 20 && window.scrollY < 120) {
+document.addEventListener('scroll', throttle(() => {
+  checkNavbar();
+  checkLearnMore();
+}, 100));
+
+window.addEventListener('load', () => {
+  checkNavbar();
+  checkLearnMore();
+});
+
+function checkNavbar() {
+  if (window.scrollY >= 20) {
     navbar.className = 'navbar-scrolled';
     navHolder.className = 'nav-holder-scrolled';
     logo.className = 'logo-scrolled';
     mochi.className = 'mochi-scrolled';
     mochiBox.className = 'mochi-lightbox-scrolled';
   }
-  else if (window.scrollY < 30) {
+  else if (window.scrollY < 20) {
     navbar.className = 'navbar';
     navHolder.className = 'nav-holder';
     logo.className = 'logo';
     mochi.className = 'mochi';
     mochiBox.className = 'mochi-lightbox';
   }
+  return;
+}
 
-});
+// if scrolled past main page, make the "learn more" box colored fusia
+function checkLearnMore() {
+  if (window.scrollY >= 700) {
+    learnMore.className = 'learn-more-scrolled'
+  }
+  else if (window.scrollY < 700) {
+    learnMore.className = 'learn-more'
+  }
+  return;
+}

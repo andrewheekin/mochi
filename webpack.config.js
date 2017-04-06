7627a8a64bf714604d13360fb0e9982827733da0
Andrew Heekin <andrewheekin@gmail.com>
2017-04-06 17:15:40 -0400

    reduce image size, add scss and sass to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 var webpack = require('webpack');
 var path = require('path');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BabiliPlugin = require('babili-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var extractSass = new ExtractTextPlugin({filename: 'css/bundle.css'});
 
 
 module.exports = {
-  entry: ['./js/scripts.js'],
+  entry: ['./js/scripts.js', './css/main.scss'],
   // entry: ['./js/scripts.js', './css/main.scss', './css/style.css'],
   devtool: 'inline-source-map',
+  // devtool: 'source-map',  
   output: {
     publicPath: '/build/',  // path to use in index.html  
     path: __dirname + '/build',  // path webpack will use
@@ -16,36 +20,24 @@ module.exports = {
     umdNamedDefine: true
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015']
-        }
-      },
-    ],
-    // rules: [
-    //   {
-    //     test: /\.css$/,
-    //     loader: ExtractTextPlugin.extract({loader: 'css-loader?importLoaders=1'}),
-    //   },
-    //   {
-    //     test: /\.scss$/,
-    //     loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-    //   }
-    // ],
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: { presets: ['es2015'] }
+    }],
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+        use: [{ loader: 'css-loader' }, { loader: 'sass-loader' }],
+        fallback: 'style-loader'
+      })
+    }]
   },
-  // plugins: [
-  //   new ExtractTextPlugin({
-  //     filename: 'css/bundle.css',
-  //     allChunks: true,
-  //   }),
-  //   new HtmlWebpackPlugin({
-  //     template: './index.html',
-  //   }),
-  // ],  
+  plugins: [
+    extractSass,
+    // new BabiliPlugin({sourceMap: false})
+  ],  
   resolve: {
     extensions: ['.js']
   }

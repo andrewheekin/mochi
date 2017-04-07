08a99caf4b8db9ff7662ba49c7d1910ab1b1e474
Andrew Heekin <andrewheekin@gmail.com>
2017-04-07 16:58:11 -0400

    mods to mobile first 2 sections

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 $(document).ready(() => {
   let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  let navbarHeight = isMobile ? 74 : 55.5;
+  let navbarHeight = isMobile ? 74 : 55.5;  // the desktop navbar is 55.5px, the mobile (<992px) navbar is 74px
 
   $('a').on('click', function(e) { // for some reason a fat arrow function breaks things here
 

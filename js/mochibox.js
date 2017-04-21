79bc69552f9a8b602e867233303ba09548aae68e
Andrew Heekin <andrewheekin@gmail.com>
2017-04-21 12:43:48 -0400

    make popups slide by

---
+++ js/mochibox.js
@@ -0,0 +1,25 @@
+// let js = document.createElement("script");
+// js.type = "text/javascript";
+// js.src = "http://localhost:8080/js/mochibox.js";
+// document.body.appendChild(js);
+
+// one-liner
+// let js = document.createElement("script"); js.type = "text/javascript"; js.src = "http://localhost:8080/js/mochibox.js"; document.body.appendChild(js);
+
+let div = document.createElement('div');
+let text = document.createTextNode('hello there folks');
+div.appendChild(text);
+document.body.appendChild(div);
+
+// document.write( '        <div class=\"my-slides\">\n' );
+// document.write( '          <img class=\"popup-background-img\" src=\"../img/skybar.jpg\">\n' );
+// document.write( '          <div class=\"popup-content\">\n' );
+// document.write( '            <span class=\"close-popup-bucket\">×</span>\n' );
+// document.write( '            <div class=\"beer-bucket-text\">Half off buckets\n' );
+// document.write( '              <br><hr style=\"color:white;\">\n' );
+// document.write( '              Today to Friday\n' );
+// document.write( '              <img class=\"restaurant-logo\" src=\"../img/restaurant-logo.gif\">\n' );
+// document.write( '            </div>\n' );
+// document.write( '            <img class=\"beer-bucket-img\" src=\"../img/beer.jpg\">\n' );
+// document.write( '          </div>\n' );
+// document.write( '        </div>  ' );

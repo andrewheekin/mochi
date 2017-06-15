c4493086559d59d5b0fb3f5faf64a636686c4e94
Andrew Heekin <andrewheekin@gmail.com>
2017-06-15 15:56:01 -0400

    update webpack config with history api fallback

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,15 @@
 var webpack = require('webpack');
 var path = require('path');
-var BabiliPlugin = require('babili-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 var extractSass = new ExtractTextPlugin({filename: 'css/bundle.css'});
 
 
 module.exports = {
   entry: ['./js/scripts.js', './css/main.scss'],
-  // entry: ['./js/scripts.js', './css/main.scss', './css/style.css'],
   devtool: 'inline-source-map',
-  // devtool: 'source-map',  
   output: {
     publicPath: '/build/',  // path to use in index.html  
-    path: __dirname + '/build',  // path webpack will use
+    path: __dirname + '/build',  // output path webpack will use
     filename: 'js/bundle.js',
     libraryTarget: 'umd',
     umdNamedDefine: true
@@ -24,7 +19,7 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
-      query: { presets: ['es2015'] }
+      query: { presets: ['es2015', 'stage-0'] }  // es2015 allows ES6, stage-0 preset allows ES7
     }],
     rules: [{
       test: /\.scss$/,
@@ -36,9 +31,11 @@ module.exports = {
   },
   plugins: [
     extractSass,
-    // new BabiliPlugin({sourceMap: false})
   ],  
   resolve: {
     extensions: ['.js']
+  },
+  devServer: {
+    historyApiFallback: true
   }
 };
